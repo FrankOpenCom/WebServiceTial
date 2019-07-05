3c3f6b88fd0a7b5a06feaac11cc7d518a42a1e02
Frank Fan <qinglin.fan@ni.com>
2019-07-05 12:17:25 +0800

    dev - staic page for submitting topics

--- source/run.py
+++ source/run.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return app.send_static_file('home.html')
+@app.route('/', methods=['GET', 'POST'])
+def submisstion():
+    if request.method == "GET":
+        return app.send_static_file('home.html')
+    else:
+        if request.method == "POST":
+            print(request.form)
+            return ""
+
 
 app.run()
